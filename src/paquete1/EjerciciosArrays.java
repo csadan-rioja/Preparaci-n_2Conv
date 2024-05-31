f183820c7357633acc2d6da04968629590b17a31
csadan-rioja <csaenz@iescomercio.com>
2024-05-31 16:07:20 +0200

    primer commit enunciados

---
+++ src/paquete1/EjerciciosArrays.java
@@ -0,0 +1,53 @@
+package paquete1;
+
+import java.util.ArrayList;
+
+public class EjerciciosArrays {
+
+	public static void main(String[] args) {
+
+
+	}
+
+	/**Función que devolverá verdadero en caso de que el número n se encuentre dentro de listaNumeros y falso en caso contrario*/
+	public static Boolean encontrarNumero(int[] listaNumeros, int n) {
+
+	}
+	/**Función que devolverá verdadero en caso de que la cadena n se encuentre dentro de listaCadenas y falso en caso contrario*/
+	public static Boolean encontrarCadena(String[] listaCadenas, String n) {
+
+	}
+	
+	/**Función que devolverá verdadero en caso de que la cadena n se encuentre dentro de listaCadenas y falso en caso contrario*/
+	public static Boolean existenDosOcurrencias(String[] listaCadenas, String n) {
+
+	}
+
+	/**Función que devolverá verdadero en caso de que alguna de las cadenas de listaCadenas se encuentre repetida*/
+	public static Boolean existenCadenaRepetidas(String[] listaCadenas) {
+
+	}
+
+	/**Función que devolverá verdadero en caso de que el número n se encuentre dentro de listaNumeros y falso en caso contrario*/
+	public static Boolean encontrarNumero2(ArrayList<Integer> listaNumeros, Integer n) {
+
+
+	}
+
+	/**Función que devolverá la suma de los números almacenados en listaNumeros*/
+	public static Integer sumarComponentes(ArrayList<Integer> listaNumeros) {
+
+	}
+
+	/**Función que devolverá la media de los números almacenados en listaNumeros*/
+	public static Double mediaComponentes(Integer[] listaNumeros) {
+
+
+	}
+
+	/**Función que devolverá verdadero si existen dos números en listaNumeros cuya suma sea 'suma' y falso en caso contrario*/
+	public static Boolean encontrarSuma(int[] listaNumeros, int suma) {
+
+	}
+
+}
